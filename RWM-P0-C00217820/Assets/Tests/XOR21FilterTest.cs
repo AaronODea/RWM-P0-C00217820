3c9dcf0170d2a831fbea2f68b81bc80ec1be9ff3
Aaron <c00217820@itcarlow.ie>
2019-09-19 17:08:26 +0100

    Xor21 filter added

---
+++ RWM-P0-C00217820/Assets/Tests/XOR21FilterTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class XOR21FilterTest
+    {
+        [Test]
+        public void XOR21Simple()
+        {
+            //Assert.IsTrue(false);
+             
+            int[] input = { 4, 2, 8, 3, 9, 4, 10 };
+            int[] output = XOR21Filter.XOR21s(input);
+            int[] expected = { 17, 23, 29, 22, 28, 17, 31 };
+
+           
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
